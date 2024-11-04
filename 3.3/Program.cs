369e1ec0d658a98b3acba91f6e41e951cd3354c8
unknown <@DESKTOP-VPI29MR>
2024-11-04 13:24:10 +0200

    Добавьте файлы проекта.

---
+++ 3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.Write("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+
+    Console.Write("Введите n:");
+    int n = int.Parse(Console.ReadLine());
+
+    double s = 0;
+
+    int F = 1;
+
+    for(int j = 1; j <= n; j += 1)
+    {
+        F = F * j;
+    }
+    Console.WriteLine($"Factorial = {F}");
+    for (int i = 1; i <= n; i += 1)
+    {
+        s += x - (Math.Pow(x, i) / F);
+    }
+    Console.WriteLine($"s = {s:F2}");
+        
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
